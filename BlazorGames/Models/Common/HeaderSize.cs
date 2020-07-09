881b711ce3ee91a3225d39f741d2595f5e276ac1
Matthew Jones <10541872+exceptionnotfound@users.noreply.github.com>
2020-07-09 10:04:47 -0700

    Added Headers, Header Links, "Back to Top" links, and Table of Contents to Minesweeper demo.

---
+++ BlazorGames/Models/Common/HeaderSize.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorGames.Models.Common
+{
+    public enum HeaderSize
+    {
+        H1,
+        H2,
+        H3,
+        H4,
+        H5,
+        H6
+    }
+}
