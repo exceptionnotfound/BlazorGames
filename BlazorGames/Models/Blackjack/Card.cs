e19884d81c2374d5ce5c933480755ff0d3be3a6b
Matthew Jones <10541872+exceptionnotfound@users.noreply.github.com>
2021-01-07 08:21:45 -0700

    Renamed property CssClass to ImageName to better reflect its purpose.

--- BlazorGames/Models/Blackjack/Card.cs
+++ BlazorGames/Models/Blackjack/Card.cs
@@ -6,42 +6,42 @@ using System.Threading.Tasks;
 
 namespace BlazorGames.Models.Blackjack
 {
-    public class Card
+public class Card
+{
+    public CardSuit Suit { get; set; }
+    public CardValue Value { get; set; }
+    public string ImageName { get; set; }
+    public bool IsVisible { get; set; } = true;
+
+    public int Score
     {
-        public CardSuit Suit { get; set; }
-        public CardValue Value { get; set; }
-        public string ImageName { get; set; }
-        public bool IsVisible { get; set; } = true;
-
-        public int Score
+        get
         {
-            get
+            if (Value == CardValue.King || Value == CardValue.Queen || Value == CardValue.Jack)
             {
-                if (Value == CardValue.King || Value == CardValue.Queen || Value == CardValue.Jack)
-                {
-                    return 10;
-                }
-                if(Value == CardValue.Ace)
-                {
-                    return 11;
-                }
-                else
-                {
-                    return (int)Value;
-                }
+                return 10;
             }
-            
+            if(Value == CardValue.Ace)
+            {
+                return 11;
+            }
+            else
+            {
+                return (int)Value;
+            }
         }
+            
+    }
 
-        public bool IsTenCard
+    public bool IsTenCard
+    {
+        get
         {
-            get
-            {
-                return Value == CardValue.Ten
-                       || Value == CardValue.Jack
-                       || Value == CardValue.Queen
-                       || Value == CardValue.King;
-            }
+            return Value == CardValue.Ten
+                    || Value == CardValue.Jack
+                    || Value == CardValue.Queen
+                    || Value == CardValue.King;
         }
     }
+}
 }
