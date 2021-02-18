6041fd6aa7803d5eb647334e8baca277881a8726
Matthew Jones <10541872+exceptionnotfound@users.noreply.github.com>
2021-02-18 12:58:50 -0700

    Added Tetris theme music

--- BlazorGames/wwwroot/js/utilities.js
+++ BlazorGames/wwwroot/js/utilities.js
@@ -1,3 +1,11 @@
 ﻿window.setTitle = (title) => {
     document.title = title + " - BlazorGames";
 }
+
+window.SetFocusToElement = (element) => {
+    element.focus();
+};
+
+window.PlayAudio = (elementName) => {
+    document.getElementById(elementName).play();
+}
