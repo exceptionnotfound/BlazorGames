6dfdc3f82162c5457f9989f44d6d8c460f89fcb9
Matthew Jones <10541872+exceptionnotfound@users.noreply.github.com>
2021-05-26 15:11:51 -0700

    Removed unnecessary methods from StackPile

--- BlazorGames/Models/Solitaire/StackPile.cs
+++ BlazorGames/Models/Solitaire/StackPile.cs
@@ -8,22 +8,6 @@ namespace BlazorGames.Models.Solitaire
 {
     public class StackPile : PileBase
     {
-        public bool CanStack(Card card)
-        {
-            var lastCard = Last();
-
-            int draggedCardValue = (int)card.Value;
-            int stackedCardValue = (int)lastCard.Value;
-
-            bool isOneLess = draggedCardValue == stackedCardValue - 1;
-
-            bool isOppositeColor = (card.Suit == CardSuit.Spades || card.Suit == CardSuit.Clubs)
-                                    ? lastCard.Suit == CardSuit.Hearts || lastCard.Suit == CardSuit.Diamonds
-                                    : lastCard.Suit == CardSuit.Spades || lastCard.Suit == CardSuit.Clubs;
-
-            return isOneLess && isOppositeColor;
-        }
-
         public int IndexOf(Card card)
         {
             var matchingCard = Cards.FirstOrDefault(x => x.Suit == card.Suit && x.Value == card.Value);
@@ -35,12 +19,7 @@ namespace BlazorGames.Models.Solitaire
 
         public bool HasNoHiddenCards()
         {
-            return !Any() || AllAreVisible();
-        }
-
-        public bool AllAreVisible()
-        {
-            return Cards.All(x => x.IsVisible);
+            return !Any() || Cards.All(x => x.IsVisible);
         }
 
         public int Count()
