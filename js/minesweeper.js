c82b91cf9e73ab659ad76113a3d4517bf8113b1a
exceptionnotfound <10541872+exceptionnotfound@users.noreply.github.com>
2021-07-01 16:15:25 +0000

    Deploying to gh-pages from master @ 42f48b31a1091d0582f9669b45d2dc26601bd1ac 🚀

---
+++ js/minesweeper.js
@@ -0,0 +1,28 @@
+﻿function faceOooh(event) {
+    if (event.button === 0) {
+        document.getElementById("face").className = "minesweeper-face-oooh";
+    }
+}
+
+function faceSmile() {
+    var face = document.getElementById("face");
+    
+    if (face !== undefined)
+        face.className = "minesweeper-face-smile";
+}
+
+function setTime(hundreds, tens, ones) {
+    var hundredsElement = document.getElementById("seconds_hundreds");
+    var tensElement = document.getElementById("seconds_tens");
+    var onesElement = document.getElementById("seconds_ones");
+
+    if (hundredsElement !== null) {
+        hundredsElement.className = "minesweeper-time-" + hundreds;
+    }
+    if (tensElement !== null) {
+        tensElement.className = "minesweeper-time-" + tens;
+    }
+    if (onesElement !== null) {
+        onesElement.className = "minesweeper-time-" + ones;
+    }
+}
---
+++ js/utilities.js
@@ -0,0 +1,44 @@
+﻿window.setTitle = (title) => {
+    document.title = title + " - BlazorGames";
+}
+
+window.SetFocusToElement = (element) => {
+    element.focus();
+};
+
+window.PlayAudio = (elementName) => {
+    document.getElementById(elementName).play();
+}
+
+window.PauseAudio = (elementName) => {
+    document.getElementById(elementName).pause();
+}
+
+window.WriteCookie = (name, value, days) => {
+    var expires;
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toGMTString();
+    }
+    else {
+        expires = "";
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+window.ReadCookie = (cname) => {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) === ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) === 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return 0;
+}
